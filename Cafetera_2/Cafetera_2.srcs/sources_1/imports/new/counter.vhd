802a5a77ad325ffc347b27d1ae7c91dc53031107
David Abad <163565363+dabadvva@users.noreply.github.com>
2025-01-14 22:20:06 +0100

    Revisión código (ignorar cambio de nombre en el proyecto)
    No se ha revisado todo el código pero: se ha puesto bien la longitud de los vectores entre módulos (CE, ASYNC IN/OUT, etc..) y se ha detectado un error de diseño en el reloj que no permite realizar la cuenta más allá de lo que dure la pulsación del botón recordando que el botón de café se traduce al final en el enable de nuestro contador, por lo que habrá que considerar esto para el cambio, además las condiciones de verificación de code_i con las cuentas máximas estaban mal y las asignaciones de ciertas señales se tienen que hacer en el procces como dejo indicado en el código que subo.
    
    Los cambios son tantos porque he tenido que abrir un proyecto nuevo por temas de compatibilidad.

---
+++ Cafetera_2/Cafetera_2.srcs/sources_1/imports/new/counter.vhd
@@ -0,0 +1,99 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 31.10.2024 17:19:29
+-- Design Name: 
+-- Module Name: counter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+use ieee.numeric_std.ALL;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+entity counter is
+    port ( 
+       CLK : in std_logic;
+       CE : in std_logic_vector(2 downto 0); -- el counter enable activa el contador
+       RST_N : in std_logic;
+       code : out std_logic_vector(7 downto 0); --Se cambia a 8b para que pueda contar hasta 150
+       EVENT_DONE : out std_logic --el led se enciende cuando se termina el tiempo de hacer un café ?
+    );
+end counter;
+
+architecture Behavioral of counter is
+    signal enable : std_logic ;
+    signal code_i : unsigned(7 downto 0);
+    constant MAX_COUNT_C : unsigned(5 downto 0) := "110010";-- cuenta hasta 10 en cafe corto
+    constant MAX_COUNT_L : unsigned(6 downto 0) := "1100100"; -- cuenta hasta 20 en cafe largo
+    constant MAX_COUNT_Le : unsigned(7 downto 0) := "10010110"; -- cuenta hasta 30 en leche
+    --Ojo, las cuentas no se están realizando en decimal, con un periodo de 100ms, que es el que
+    --utilizamos, tras 50 rising edge se cumplen 10s, en decimal 50 es 110010, y así con los demás 
+begin
+    enable <= CE(0) or CE(1) or CE(2); -- Combina las señales de entrada, si una está activa, el contador empieza
+    process(CLK, RST_N)
+    variable evnt : std_logic ;
+    begin
+        if RST_N = '0' then --reset del contador
+            code_i <=(others => '0');
+        elsif rising_edge(CLK) then 
+            if enable = '1'  then 
+                case CE is
+                    when "001" =>  -- corto, contar hasta 10
+                        if code_i < ("00" & MAX_COUNT_C) then
+                            code_i <= code_i + 1;  -- Incrementa el contador
+                        else 
+                            code <= std_logic_vector(code_i);
+                        end if;
+                    when "010" =>  -- largo, contar hasta 20
+                        if code_i < ("0" & MAX_COUNT_L) then
+                            code_i <= code_i + 1;
+                        else 
+                            code <= std_logic_vector(code_i);
+                        end if;
+                    when "100" =>  -- leche, contar hasta 30
+                        if code_i < MAX_COUNT_Le then
+                            code_i <= code_i + 1;
+                        else 
+                            code <= std_logic_vector(code_i);
+                        end if;
+                    when others =>  -- Si CE no es ninguno de los anteriores, no contar
+                        code_i <= code_i;  -- Mantiene el contador en su valor actual
+                end case;
+                
+                case code_i is
+                    when "00" & MAX_COUNT_C =>
+                        evnt := '1';
+                    when "0" & MAX_COUNT_L =>
+                        evnt := '1';
+                    when MAX_COUNT_Le =>
+                        evnt := '1';
+                    when others =>
+                        evnt := '0';      
+                 end case; 
+                 EVENT_DONE <= evnt; 
+            end if;
+        end if;
+    end process;
+    
+end Behavioral;
+
